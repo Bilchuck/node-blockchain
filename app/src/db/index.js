76789ab1336f2f5a7799434c0037798029698886
Bilchuck <anton.bilchuk@stylelounge.com>
2018-06-06 16:57:03 +0300

    fix: major error with no mysql detection

--- app/src/db/index.js
+++ app/src/db/index.js
@@ -4,11 +4,9 @@ const dbName = 'blockchain'
 const user = 'root'
 const password = 'secret'
 const host = process.env.DATABASE_HOST || '127.0.0.1'
-const port = '3306'
 const dialect = 'mysql'
 
 module.exports = new Sequelize(dbName, user, password, {
   host,
-  port,
   dialect
 })
